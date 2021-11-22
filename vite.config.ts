d25ab00f50f5cd7f9b52177642b408859fe41f14
Ron Mizrahi <sronmiz@gmail.com>
2021-11-22 14:44:10 +0200

    add path resolver

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Pages from "vite-plugin-pages";
 import Layouts from "vite-plugin-vue-layouts";
-import WindiCSS from 'vite-plugin-windicss'
+import WindiCSS from "vite-plugin-windicss";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@/": `${path.resolve(__dirname, "src")}/`,
+    },
+  },
   plugins: [
     vue(),
     WindiCSS(),
